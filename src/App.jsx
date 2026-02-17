45b4d5d9bcbf9382ebdb0dcc0ba200e6009e6567
hatfelicien <hategekimanafelicien65.com>
2026-02-17 17:20:07 +0000

    Fix SPA routing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 import ModuleView from './pages/ModuleView';
-// Placeholders for now, will implement next
 import LessonView from './pages/LessonView';
 import QuizView from './pages/QuizView';
-
-// Temporary placeholders if files don't exist yet to avoid crash during build steps
-const LessonPlaceholder = () => <div>Lesson View</div>;
-const QuizPlaceholder = () => <div>Quiz View</div>;
 
 function App() {
   return (
